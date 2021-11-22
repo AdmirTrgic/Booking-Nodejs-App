47c299357171b7d549c99a495e7500fe1a003569
Admir Trgic <admir.t@gmail.com>
2021-11-22 15:17:56 +0100

    Update signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
-
+router.get('/signup', authController.signup);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);

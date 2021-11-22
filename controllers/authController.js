70f8db0b5cdeb587269d2b31ecf14eed9f80cc09
Admir Trgic <admir.t@gmail.com>
2021-11-22 10:23:30 +0100

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,7 +55,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
